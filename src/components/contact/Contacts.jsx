59f380dd3d0fa4d1518348c30614366b6072cf00
ali <alimonfared484@gmail.com>
2024-06-21 01:44:45 +0330

    create the add contact button , contacts component and contact component

--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,11 +1,38 @@
+import {
+  CURRENTTLINE,
+  CYAN,
+  ORANGE,
+  PINK,
+  PURPLE,
+  RED,
+} from "../../helpers/colors";
 
+import Contact from "./Contact";
 
 const Contacts = () => {
-    return(
-        <>
-            <h3>Contacts</h3>
-        </>
-    )
-}
+  return (
+    <>
+      <section className="container">
+        <div className="grid">
+          <div className="row">
+            <div className="col">
+              <p className="h3">
+                <button className="btn mx-2" style={{ backgroundColor: PINK }}>
+                  ساخت مخاطب جدید
+                  <i className="fa fa-plus-circle mx-2" />
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="container">
+        <div className="row">
+            <Contact/>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Contacts;
