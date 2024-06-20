a67c109780f9a32885827c2f753a19bcfe94b014
ali <alimonfared484@gmail.com>
2024-06-20 16:55:28 +0330

    Building the initial structure of the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 style={{fontFamily: "Vazir"}}>ابزار مدیریت مخاطبین</h1>
+        <button className='btn btn-primary'>Button</button>
+    </div>
+  );
+}
+
+export default App;
