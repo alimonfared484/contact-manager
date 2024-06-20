f8e07e0475de6a0effd99ca9ece44361e727f74f
ali <alimonfared484@gmail.com>
2024-06-20 17:31:56 +0330

    create the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1 style={{fontFamily: "Vazir"}}>ابزار مدیریت مخاطبین</h1>
-        <button className='btn btn-primary'>Button</button>
+        <Navbar/>
     </div>
   );
 }
