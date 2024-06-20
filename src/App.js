7ec97eb2c3847296e52ee7569219d505de57b0bb
ali <alimonfared484@gmail.com>
2024-06-20 20:57:23 +0330

    create the basic colors file and set that to website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Navbar from './components/Navbar';
+import Contacts from './components/contact/Contacts';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Contacts/>
     </div>
   );
 }
---
+++ src/components/contact/Contacts.jsx
@@ -0,0 +1,11 @@
+
+
+const Contacts = () => {
+    return(
+        <>
+            <h3>Contacts</h3>
+        </>
+    )
+}
+
+export default Contacts;
